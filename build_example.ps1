59a32ff5906d1f7f4a2b3772d2c1adce11d459af
Marko Grdinić <mrakgr@gmail.com>
2025-06-16 15:51:09 +0200

    Dug out these flags from the makefile and the example actually compiles. Now I need to progressively eliminate them to figure out which one of them is responsible for allowing the compilation to succeed.

--- build_example.ps1
+++ build_example.ps1
@@ -13,13 +13,23 @@ nvcc `
     -I"cutlass/tools/util/include" `
     -isystem /usr/local/cuda/include `
     -isystem /usr/local/cuda/include/cccl `
-    -arch=native `
-    -D=NDEBUG `
-    -g -G `
-    -restrict `
-    -expt-relaxed-constexpr `
-    -D__CUDA_NO_HALF_CONVERSIONS__ `
-    -std=c++20 `
+    -DCUTLASS_VERSIONS_GENERATED `
+    -O3 `
+    -DNDEBUG `
+    -std=c++17 `
+    --generate-code=arch=compute_120a,code=[sm_120a] `
+    --generate-code=arch=compute_120a,code=[compute_120a] `
+    -Xcompiler=-fPIE `
+    -DCUTLASS_ENABLE_TENSOR_CORE_MMA=1 `
+    -DCUTLASS_ENABLE_GDC_FOR_SM100=1 `
+    --expt-relaxed-constexpr `
+    -ftemplate-backtrace-limit=0 `
+    -DCUTLASS_TEST_LEVEL=0 `
+    -DCUTLASS_TEST_ENABLE_CACHED_RESULTS=1 `
+    -DCUTLASS_CONV_UNIT_TEST_RIGOROUS_SIZE_ENABLED=1 `
+    -DCUTLASS_DEBUG_TRACE_LEVEL=0 `
+    -Xcompiler=-Wconversion `
+    -Xcompiler=-fno-strict-aliasing `
     -o $output_file `
     $example_file
 
