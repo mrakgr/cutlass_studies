215625d75ed3290ae712827c31487cf60e5280e6
Marko Grdinić <mrakgr@gmail.com>
2025-06-16 16:16:05 +0200

    Let me try it with just `-lineinfo`.

--- build_example.ps1
+++ build_example.ps1
@@ -13,16 +13,15 @@ nvcc `
     -I"cutlass/tools/util/include" `
     -isystem /usr/local/cuda/include `
     -isystem /usr/local/cuda/include/cccl `
-    -DCUTLASS_VERSIONS_GENERATED `
-    -g -G `
-    -DNDEBUG `
+    -g -lineinfo `
+    -arch=native `
     -std=c++17 `
-    --generate-code=arch=compute_120a,code=[sm_120a] `
-    --generate-code=arch=compute_120a,code=[compute_120a] `
+    --expt-relaxed-constexpr `
+    -DCUTLASS_VERSIONS_GENERATED `
+    -DNDEBUG `
     -Xcompiler=-fPIE `
     -DCUTLASS_ENABLE_TENSOR_CORE_MMA=1 `
     -DCUTLASS_ENABLE_GDC_FOR_SM100=1 `
-    --expt-relaxed-constexpr `
     -ftemplate-backtrace-limit=0 `
     -DCUTLASS_TEST_LEVEL=0 `
     -DCUTLASS_TEST_ENABLE_CACHED_RESULTS=1 `
